92e182b810a4b7b9dcbbc7a7d98be3ba36c30782
ZydrunasK <kniukstaz@gmail.com>
2024-10-07 19:07:52 +0300

    up

--- api/scheduleRouter.js
+++ api/scheduleRouter.js
@@ -2,12 +2,14 @@ import e from "express";
 import { isValidWeekSchedule } from "../lib/isValidWeekSchedule.js";
 import { isValidDaySchedule } from "../lib/isValidDaySchedule.js";
 import { isValidLesson } from "../lib/isValidLesson.js";
+import { isValidId } from "../lib/isValidId.js";
 
 let week = {};
 
 export const scheduleRouter = e.Router();
 
 scheduleRouter.post('/', (req, res) => {
+
     const [isValid, msg] = isValidWeekSchedule(req.body);
     if (isValid) {
         week = req.body ;
@@ -23,41 +25,72 @@ scheduleRouter.post('/', (req, res) => {
 });
 
 scheduleRouter.get('/', (req, res) => {
+
     if (Object.keys(week).length === 1) {
         return res.status(200).json(week);
     }
+
     return res.status(400).json({
         status: 'error',
-        msg: 'nera ko grazinti'
+        msg: 'nera tvarkarascio'
     });
 });
 
 scheduleRouter.get('/:dienosID', (req, res) => {
-    if (Array.isArray(week.schedule[req.params.dienosID - 1])) {
-        return res.status(200).json(week.schedule[req.params.dienosID - 1]);
+    
+    const [isValidDayId, idMsg, id] = isValidId(req.params.dienosID);
+    if (!isValidDayId) {
+        return res.status(400).json({
+            status: 'error',
+            msg: idMsg
+        });
     }
-    return res.status(400).json({
-        status: 'error',
-        msg: 'nera ko grazinti'
-    });
+
+    if (!Object.keys(week).includes('schedule')) {
+        return res.status(400).json({
+            status: 'error',
+            msg: 'nera tvarkarascio'
+        });
+    }
+    return res.status(200).json(week.schedule[id]);
 });
 
 scheduleRouter.put('/:dienosID', (req, res) => {
-    const [isValid, msg] = isValidDaySchedule(req.body);
-    if (isValid) {
-        week.schedule[req.params.dienosID - 1] = req.body.schedule;
-        return res.status(200).json({
-            status: 'success',
-            msg: 'dienos pamokos pakeistos'
+    
+    const [isValidDayId, idMsg, id] = isValidId(req.params.dienosID);
+    if (!isValidDayId) {
+        return res.status(400).json({
+            status: 'error',
+            msg: idMsg
         });
     }
-    return res.status(400).json({
-        status: 'error',
-        msg: msg
+
+    const [DayisValid, dayMsg] = isValidDaySchedule(req.body);
+    if (!DayisValid) {
+        return res.status(400).json({
+            status: 'error',
+            msg: dayMsg
+        });
+    }
+
+    week.schedule[id] = req.body.schedule;
+    return res.status(200).json({
+        status: 'success',
+        msg: 'dienos pamokos pakeistos'
     });
 });
 
 scheduleRouter.patch('/:dienosID/:pamokosID', (req, res) => {
+    
+    const [isValidDayId, dayMsg, dayId] = isValidId(req.params.dienosID);
+    const [isValidLessonId, lessonMsg, lessonId] = isValidId(req.params.pamokosID);
+    if (!isValidDayId
+        || !isValidLessonId) {
+        return res.status(400).json({
+            status: 'error',
+            msg: (isValidDayId ? lessonMsg : dayMsg)
+        });
+    }
     
     if (Object.keys(week).length !== 1) {
         return res.status(400).json({
@@ -66,40 +99,41 @@ scheduleRouter.patch('/:dienosID/:pamokosID', (req, res) => {
         });
     }
 
-    const dienosID = req.params.dienosID - 1;
-    const pamokosID = req.params.pamokosID - 1;
-    if (week.schedule[dienosID][pamokosID] === undefined) {
-        return res.status(400).json({
-            status: 'error',
-            msg: `nera pamokos ${pamokosID + 1}`
-        });
-    }
     
     const [isValid, msg] = isValidLesson(req.body);
-    if (isValid) {
-        week.schedule[dienosID][pamokosID] = req.body.lesson;
-        return res.status(200).json({
-            status: 'success',
-            msg: `pamoka ${pamokosID + 1} pakeista`
+    if (!isValid) {
+        return res.status(400).json({
+            status: 'error',
+            msg: msg
         });
     }
-    return res.status(400).json({
-        status: 'error',
-        msg: msg
+
+    week.schedule[dayId][lessonId] = req.body.lesson;
+    return res.status(200).json({
+        status: 'success',
+        msg: `dienos ${dayId + 1} pamoka ${lessonId + 1} pakeista`
     });
 });
 
 scheduleRouter.delete('/:dienosID', (req, res) => {
-    if (Object.keys(week).length === 1) {
-        const dienosID = req.params.dienosID - 1;
-        week.schedule[dienosID] = [];
-        return res.status(200).json({
-            status: 'success',
-            msg: `dienos pamokos istrintos`
+    const [isValidDayId, idMsg, id] = isValidId(req.params.dienosID);
+    if (!isValidDayId) {
+        return res.status(400).json({
+            status: 'error',
+            msg: idMsg
         });
     }
-    return res.status(400).json({
-        status: 'error',
-        msg: 'nera ko trinti'
+
+    if (Object.keys(week).length !== 1) {
+        return res.status(400).json({
+            status: 'error',
+            msg: 'nera ko trinti'
+        });
+    }
+
+    week.schedule[dienosID] = [];
+    return res.status(200).json({
+        status: 'success',
+        msg: `dienos ${id + 1} pamokos istrintos`
     });
 });
